d97484d0858cf9ea91926e59a91f230ceef689b1
Shakhrii <sh.ramaldanova@gmail.com>
2025-01-20 02:50:16 +0300

    feat: complete 2nd task

---
+++ 02-write-file/index.js
@@ -0,0 +1,32 @@
+const readline = require('readline');
+const { stdin: input, stdout: output } = require('process');
+const fs = require('fs');
+const path = require('path');
+
+const rl = readline.createInterface({ input, output });
+const pathFile = path.join(__dirname, 'input.txt');
+const ws = fs.createWriteStream(pathFile);
+
+rl.setPrompt('Type something you want');
+rl.prompt();
+rl.on('line', (input) => {
+  if (input.trim() === 'exit') {
+    goodbye();
+  } else {
+    saveToFile(input);
+  }
+});
+
+rl.on('SIGINT', () => {
+  goodbye();
+});
+
+const goodbye = () => {
+  console.log('Goodbye!');
+  rl.close();
+  ws.end();
+};
+
+const saveToFile = (input) => {
+  ws.write(input);
+};
