fcf5f6f6e2f74c9edb08e6a2121d884e48265ca7
Shakhrii <sh.ramaldanova@gmail.com>
2025-01-20 20:03:22 +0300

    refactor: add async await

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -3,41 +3,41 @@ const path = require('path');
 const srcPath = path.join(__dirname, 'files');
 const destPath = path.join(__dirname, 'files-copy');
 
-const copyFolder = (srcPath, destPath) => {
+async function copyFolder(srcPath, destPath) {
   fs.mkdir(destPath, { recursive: true }, (err) => {
     if (err) {
       return console.error(err);
     }
   });
 
-  fs.readdir(srcPath, (err, files) => {
-    if (err) {
-      console.log(err);
+  let files = await getFilesFromFolder(srcPath);
+  for (let file of files) {
+    let filePath = path.join(srcPath, file);
+    if (await isFile(filePath)) {
+      fs.copyFile(
+        path.join(srcPath, file),
+        path.join(destPath, file),
+        (err) => {
+          if (err) {
+            console.error(err);
+          }
+        },
+      );
     } else {
-      files.forEach((file) => {
-        let filePath = path.join(srcPath, file);
-
-        let promise = isFile(filePath);
-        promise.then(
-          (isFile) => {
-            if (isFile) {
-              fs.copyFile(
-                path.join(srcPath, file),
-                path.join(destPath, file),
-                (err) => {
-                  if (err) {
-                    console.error(err);
-                  }
-                },
-              );
-            } else {
-              copyFolder(filePath, path.join(destPath, file));
-            }
-          },
-          (error) => console.log(error),
-        );
-      });
+      copyFolder(filePath, path.join(destPath, file));
     }
+  }
+}
+
+const getFilesFromFolder = (srcPath) => {
+  return new Promise(function (resolve, reject) {
+    fs.readdir(srcPath, (err, files) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(files);
+      }
+    });
   });
 };
 
